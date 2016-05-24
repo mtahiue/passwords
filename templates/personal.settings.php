3ecb37b00228ccb4ee546c8e78c208ca0d420931
Fallon Turner <fcturner@users.noreply.github.com>
2016-05-24 11:48:08 +0200

    Update personal.settings.php

--- templates/personal.settings.php
+++ templates/personal.settings.php
@@ -4,6 +4,7 @@ namespace OCA\Passwords;
 
 \OCP\Util::addStyle('passwords', 'settings');
 \OCP\Util::addScript('passwords', 'settings');
+\OCP\Util::addScript('passwords', 'sha512');
 
 $tmpl = new \OCP\Template('passwords', 'part.personal');
 
