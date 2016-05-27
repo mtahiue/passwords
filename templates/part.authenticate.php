ada354811349e82afc52717ea69a8f39d66d8641
Fallon Turner <fcturner@users.noreply.github.com>
2016-05-27 20:30:14 +0200

    Update part.authenticate.php

--- templates/part.authenticate.php
+++ templates/part.authenticate.php
@@ -12,13 +12,15 @@
 ?>
 <div id="auth_div">
 	<h2><?php p($l->t('Authenticate')); ?>:</h2>
-	<input id="auth_pass" type="password" placeholder="<?php p($auth_type); ?>"><br>
-	<?php
-	if (isset($_GET['token'])) { ?>
-		<p id="invalid_auth"><?php p($l->t('This password is invalid. Please try again.')); ?></p>
-	<?php }	?>
-	<input class="button primary" type="button" id="auth_btn" value="<?php p($l->t('Authenticate')); ?>">
-	<p><?php p($l->t('You need to authenticate using a password.')); ?> <a href="/index.php/settings/personal"><?php p($l->t('This can be changed in your settings')); ?></a>.</p>
+	<form id="auth_form">
+		<input id="auth_pass" type="password" placeholder="<?php p($auth_type); ?>"><br>
+		<?php
+		if (isset($_GET['token'])) { ?>
+			<p id="invalid_auth"><?php p($l->t('This password is invalid. Please try again.')); ?></p>
+		<?php }	?>
+		<input class="button primary" type="submit" id="auth_btn" value="<?php p($l->t('Authenticate')); ?>">
+		<p><?php p($l->t('You need to authenticate using a password.')); ?> <a id="href_settings"><?php p($l->t('This can be changed in your settings')); ?></a>.</p>
+	</form>
 </div>
 <div id="auth_footer">
 	<p id="githubref"><a href="https://github.com/fcturner/passwords/" target="_blank"><?php p($instancename . ' ' . $passwordsname) ?></a>, v<?php p($passwordsversion) ?></p>
