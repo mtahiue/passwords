7127404608cf924e0e2f8b6e362c942956f0a569
Fallon Turner <fcturner@users.noreply.github.com>
2016-05-27 20:33:26 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
 
+	$("#href_settings").attr("href", OC.generateUrl('/settings/personal#' + t('passwords', 'Passwords').toLowerCase()));
+
 	$('#auth_btn').val(t('core', 'Continue'));
 	$('#auth_pass').focus();
-
-	$('#auth_btn').click(function() {
-		var uid = $('head').attr('data-user');
-		window.location = OC.generateUrl('/apps/passwords/') + '?token=' + SHA512(uid + SHA512($('#auth_pass').val()));
+	
+	$("#auth_form").on("submit", function() {
+		if ($('#auth_pass').val() == '') {
+			return false;
+		}
+		if ($('#auth_pass').attr('placeholder') == t('passwords', 'Master password')) {
+			var uid = $('head').attr('data-user');
+			window.location = OC.generateUrl('/apps/passwords/') + '?token=' + SHA512(uid + SHA512($('#auth_pass').val()));
+			return false;
+		} else {
+			alert('OC pass not yet implemented...');
+		}
+		return false;
 	});
 
 	setTimeout(function() {
