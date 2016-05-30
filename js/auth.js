7c240bd28ec7d487159bb77d49b2c8ab906de231
Fallon Turner <fcturner@users.noreply.github.com>
2016-05-30 21:51:41 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,42 @@
 $(document).ready(function() {
 
+	// initialize authorization model
+	var Auth = function(baseUrl) {
+		this._baseUrl = baseUrl;
+		this._auth = [];
+	};
+
+	Auth.prototype = {
+		checkAuth: function(pass, type) {
+			var result = 'error';
+			var request = $.ajax({
+				url: this._baseUrl + '/' + pass + '/' + type,
+				method: 'POST',
+				async: false
+			});
+			
+			request.done(function(msg) {
+				// will be 'pass' or 'fail';
+				result = msg;
+				if (result == 'pass') {
+					// create cookie on 'pass'
+					setCookie();
+				}
+			});
+ 
+			request.fail(function( jqXHR, textStatus ) {
+				alert( "Error while authenticating: " + textStatus );
+			});
+			
+			return result;
+		}
+	};
+
+	if (getCookie() == SHA512($('head').attr('data-user'))) {
+		window.location = OC.generateUrl('/apps/passwords/');
+		return false;
+	}
+
 	$("#href_settings").attr("href", OC.generateUrl('/settings/personal#' + t('passwords', 'Passwords').toLowerCase()));
 
 	$('#auth_btn').val(t('core', 'Continue'));
@@ -9,18 +46,80 @@ $(document).ready(function() {
 		if ($('#auth_pass').val() == '') {
 			return false;
 		}
-		if ($('#auth_pass').attr('placeholder') == t('passwords', 'Master password')) {
-			var uid = $('head').attr('data-user');
-			window.location = OC.generateUrl('/apps/passwords/') + '?token=' + SHA512(uid + SHA512($('#auth_pass').val()));
-			return false;
-		} else {
-			alert('OC pass not yet implemented...');
+
+		var auth = new Auth(OC.generateUrl('/apps/passwords/auth'));
+		var auth_type =  $('#auth_pass').attr('auth-type');
+		
+		var authenticate = auth.checkAuth($('#auth_pass').val(), auth_type);
+		if (authenticate == 'pass') {
+			// has auth cookie, so go for it
+			window.location = OC.generateUrl('/apps/passwords/');
+		} else if (authenticate == 'fail') {
+			$('#invalid_auth').slideDown(200);
+			setTimeout(function() {
+				$('#invalid_auth').slideUp(500);
+			}, 4500);
 		}
+
 		return false;
 	});
 
-	setTimeout(function() {
-		$('#invalid_auth').slideUp(500);
-	}, 4500);
-
 });
+
+function setCookie() {
+	// initialize settings
+	var Settings = function(baseUrl) {
+		this._baseUrl = baseUrl;
+		this._settings = [];
+	};
+
+	Settings.prototype = {
+		load: function() {
+			var deferred = $.Deferred();
+			var self = this;
+			$.ajax({
+				url: this._baseUrl,
+				method: 'GET',
+				async: false
+			}).done(function( settings ) {
+				self._settings = settings;
+			}).fail(function() {
+				deferred.reject();
+			});
+			return deferred.promise();
+		},
+		getKey: function(key) {
+			for (var k in this._settings)
+			{
+				if (k == key)
+					return this._settings[k];
+			}
+		},
+		getAll: function() {
+			return this._settings;
+		}
+	};
+
+	var settings = new Settings(OC.generateUrl('/apps/passwords/settings'));
+	settings.load();
+	var seconds = settings.getKey('auth_timer');
+	var user = $('head').attr('data-user');
+	var d = new Date();
+	d.setTime(d.getTime() + (seconds * 1000));
+	var expires = "expires="+ d.toUTCString();
+	document.cookie = "oc_passwords_auth=" + SHA512(user) + "; " + expires + "; secure";
+}
+function getCookie() {
+	var name = "oc_passwords_auth=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return '';
+}
